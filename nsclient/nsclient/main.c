405ffcc8557064dd06e7cceb03e5d379f3d460e1
nabdelka <60327943+nabdelka@users.noreply.github.com>
2020-04-07 18:03:48 +0300

    starting nsclient

---
+++ nsclient/nsclient/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main() {
+
+	// TODO Add argv, and check input argument (syntax, number)
+	
+
+	// TODO call main_program
+
+}
+
+// TODO MAIN_PROGRAM
+//	- request input from user:
+//		nsclient >
+//	- check input synatx
+//		https://moodle.tau.ac.il/mod/forum/discuss.php?d=69030
+//	- if good -> call dnsQuery
+//	- else error msg
+
+// TODO dnsQuery
+//	-	got a string
+//	- return
+
+
+//  https://moodle.tau.ac.il/mod/forum/discuss.php?d=72022

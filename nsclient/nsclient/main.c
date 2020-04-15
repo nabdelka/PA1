455f7fab95cb28858365b8e7b2744561519c7c71
Ameer Kayal <ameerkayal@mail.tau.ac.il>
2020-04-15 23:07:03 +0300

    ameer_ip_checker

--- nsclient/nsclient/main.c
+++ nsclient/nsclient/main.c
@@ -8,7 +8,8 @@ int main(int argc, char *argv[]) {
 		//return -1; TODO enable
 	}
 	// TODO - do we need to check input syntax?
-	
+	//check ip
+	ip_checker(argv);
 
 	//return main_program(argv[1]); TODO enable
 	return main_program("10.0.0.138");
@@ -17,4 +18,26 @@ int main(int argc, char *argv[]) {
 
 	
 }
+void ip_checker(char *header) {
+	int i, num=0 ,points = 3, nums = 4;
+	while (header[i] != '\0') {
+		if (header[i] != '.') {
+			num = num * 10 + (header[i] - '0');
+		}
+		else {
+			if (num < 0 || num>255) {
+				printf("unvalid ip num!");
+				return;
+			}
+		}
+		points = points - 1;
+		nums = nums - 1;
+		if (points < 0 || nums<0) {
+			printf("unvalid ip num!");
+			return;
+		}
+	}
+	return;
+}
+
 

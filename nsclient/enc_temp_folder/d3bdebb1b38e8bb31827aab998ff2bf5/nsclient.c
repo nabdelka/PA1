5e4ffdddbbf7fcd89d1ceb166600462a041a3dd4
Ameer Kayal <ameerkayal@mail.tau.ac.il>
2020-04-15 15:08:29 +0300

    noora

--- nsclient/enc_temp_folder/d3bdebb1b38e8bb31827aab998ff2bf5/nsclient.c
+++ nsclient/enc_temp_folder/d3bdebb1b38e8bb31827aab998ff2bf5/nsclient.c
@@ -61,8 +61,6 @@ void dnsQuery(char *host_name) { // TODO change return type to struct
 	header_array[NSCOUNT] = 0;
 	header_array[ARCOUNT] = 0;
 
-
-	printf("%d\n", header_array[ID]);
 	char send_buff[512];
 	for (int j = 0; j < LINES_IN_HEADER; j++) {
 		send_buff[2 * j] = header_array[j] >> 8;
@@ -149,13 +147,13 @@ void header_checker(unsigned char *header) {
 	char NSCOUNT0, NSCOUNT1 , NSCOUNT;
 	char ARCOUNT0, ARCOUNT1 , ARCOUNT;
 	while(i <12){
+
 		if (i == 0) {
 			id0 = header[i];
 		}
 		if (i == 1) {
 			id1 = header[i];
 			id = id0 || id1;
-
 		}
 		if (i == 2) {
 			QR = header[i] & 0x80;
@@ -180,28 +178,28 @@ void header_checker(unsigned char *header) {
 		}
 		if (i == 5) {
 			QDCOUNT1 = header[i];
-			QDCOUNT = QDCOUNT1 || QDCOUNT0;
+			//QDCOUNT = QDCOUNT1 || QDCOUNT0;
 		}
 		if (i == 6) {
 			ANCOUNT0 = header[i];
 		}
 		if (i == 7) {
 			ANCOUNT1 = header[i];
-			ANCOUNT = ANCOUNT1 || ANCOUNT0;
+			//ANCOUNT = ANCOUNT1 || ANCOUNT0;
 		}
 		if (i == 8) {
 			NSCOUNT0 = header[i];
 		}
 		if (i == 9) {
 			NSCOUNT1 = header[i];
-			NSCOUNT = NSCOUNT0 || NSCOUNT1;
+			//NSCOUNT = NSCOUNT0 || NSCOUNT1;
 		}
 		if (i == 10) {
 			ARCOUNT0 = header[i];
 		}
 		if (i == 11) {
 			ARCOUNT1 = header[i];
-			ARCOUNT = ARCOUNT0 || ARCOUNT1 ;
+			//ARCOUNT = ARCOUNT0 || ARCOUNT1 ;
 		}
 		i = i + 1;
 	}
@@ -209,7 +207,7 @@ void header_checker(unsigned char *header) {
 	printf("\n");
 	printf("HEADER CHECK:\n");
 
-	printf("%d\n", id);
+	printf("%d\n", ARCOUNT1);
 
 	return;
 }
